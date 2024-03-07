7e737f5f031370916da5eb5b011ea3983fdbb5f8
Alex Rubio <alexblans@hotmail.com>
2024-03-07 15:52:05 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll",()=>{
+    header.classList.toggle("sticky", this.window.scrollY > 80);
+});
+
+
+///// OPEN MENU
+
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () =>{
+    menu.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+
+}
+
+/// solucion
+
+window.onscroll = ()=>{
+    menu.classList.remove("bx-x")
+    navlist.classList.remove("open")
+}
+
+
+///SCROLLReveal
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "85px",
+    duration: 2500,
+    reset: false
+});
+
+
+sr.reveal(".home-text",{delay:300});
+sr.reveal(".home-img",{delay:400});
+
+sr.reveal(".container",{delay:400});
+
+sr.reveal(".about-img",{});
+sr.reveal(".about-text",{delay:400});
+
+sr.reveal(".middle-text",{});
+sr.reveal(".row-btn, .shop-content",{delay:300});
+
+sr.reveal(".review, .contact",{delay:300});
+
